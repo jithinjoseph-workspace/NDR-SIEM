f8aa55ff0e3e5fd8e1fbae9543d5e4e9f5be829e
jithinjoseph-workspace <jithinjoseph@promatas.org>
2026-05-20 16:29:24 +0530

    added tenant admin page

---
+++ ndr-ui/src/pages/tenant-admin/tenant-admin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tenant-admin',
+  imports: [],
+  templateUrl: './tenant-admin.html',
+  styleUrl: './tenant-admin.css',
+})
+export class TenantAdmin {
+
+}
